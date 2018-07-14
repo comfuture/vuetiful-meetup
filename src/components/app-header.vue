0788933e58252ca0002a7099b63ffa4d875c631c
Unknown <comfuture@gmail.com>
2018-07-14 23:34:17 +0900

    improve app-header

--- src/components/app-header.vue
+++ src/components/app-header.vue
@@ -4,7 +4,17 @@
       <nuxt-link :to="{name: 'index'}" class="header item">
         Vuetiful Meetup
       </nuxt-link>
-      <nuxt-link :to="{name: 'organize'}" class="item">Organize</nuxt-link>
+      <nuxt-link :to="{name: 'organize'}" class="item">밋업 구성</nuxt-link>
+      <div class="right menu">
+        <nuxt-link :to="{name: 'login'}" class="item" v-if="!$store.getters.user">로그인</nuxt-link>
+        <div class="ui simple dropdown item" v-else>
+          {{$store.getters.user.email}} <i class="dropdown icon"></i>
+          <div class="menu">
+            <a class="item" href="#">프로필</a>
+            <a class="item" href="#">로그아웃</a>
+          </div>
+        </div>
+      </div>
     </div>
   </header>
 </template>
